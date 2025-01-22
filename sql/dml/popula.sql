e8cd22136b2ecc229ac8a2441395999be7690063
José_Viniciusq <viniciusa.s.queiroz@gmail.com>
2025-01-22 18:54:23 -0300

    arrumando coordenadas

--- sql/dml/popula.sql
+++ sql/dml/popula.sql
@@ -10,9 +10,7 @@ VALUES
 -- mapa 
 INSERT INTO Mapa (idMapa, descricaoMapa, nome) 
 VALUES 
-(1, 'Hogwarts é uma escola de magia icônica localizada em um castelo imponente, escondido nas montanhas da Escócia. A escola é cercada por mistério e magia, com torres majestosas, escadarias que mudam de lugar, salões encantados e terrenos vastos.', 'Hogwarts'),
-(2, 'O Beco Diagonal é uma rua mágica e movimentada localizada no coração de Londres. Ao entrar, o visitante é imediatamente imerso em um mundo de magia e encantamento. As lojas são espalhadas ao longo da rua, todas com fachadas encantadas e vitrines repletas de itens mágicos, como varinhas, poções, livros de feitiçaria e vestuários.', 'Beco Diagonal'),
-(3, 'A estação de trem em Londres, se torna um local mágico e cheio de mistério quando os bruxos e bruxas se dirigem para Hogwarts. A plataforma 9 ¾ é o ponto de partida para a jornada para a escola de magia.', 'Estacao');  
+(1, 'Hogwarts é uma escola de magia icônica localizada em um castelo imponente, escondido nas montanhas da Escócia. A escola é cercada por mistério e magia, com torres majestosas, escadarias que mudam de lugar, salões encantados e terrenos vastos.', 'Hogwarts');
 
 
 -- regioes 
@@ -20,31 +18,48 @@ INSERT INTO Regiao (idMapa,idRegiao,nome,descricaoRegiao)
 VALUES 
 (1,1,'Escola','escola de magia e bruxaria da Hogwarts'),
 (1,2,'Exterior de Hogwarts','cercado por uma paisagem encantadora e cheia de mistérios.'),
-(3,3,'Estacao 9 3/4','Estacao de trem secreta que leva para a escola de bruxaria de Hogwarts');
+(1,3,'Segundo andar do castelo','wdfqwdw');
 
 -- Area 
 INSERT INTO Area (idArea, idRegiao, nome, areaNorte, areaSul, areaLeste, areaOeste, descricaoArea) 
 VALUES
 (1, 1, 'Nada', 1, 1, 1, 1, 'Área sem definição específica.'),
-(2, 2, 'Portao de Hogwarts', 3, 4, 5, 6, 'Portão principal da escola de magia e bruxaria.'),
+(2, 2, 'Entrada principal do castelo', 21, 3, NULL,NULL, 'Portão principal da escola de magia e bruxaria.'),
 (3, 2, 'Patio exterior', 2, 1, 1, 1, 'Pátio ao ar livre utilizado para eventos e encontros.'),
-(4, 1, 'Salao Principal', 12, 11, 13, 2, 'Local de reuniões e celebrações, com longas mesas.'),
+(4, 1, 'Salao Principal', NULL, 21, NULL, NULL, 'Local de reuniões e celebrações, com longas mesas.'),
 (5, 1, 'Clareira', 1, 6, 1, 1, 'Clareira aberta na floresta, com vegetação baixa.'),
 (6, 1, 'Torre leste', 5, 1, 22, 23, 'Torre localizada no lado leste da escola.'),
 (7, 1, 'Torre oeste', 1, 5, 20, 21, 'Torre localizada no lado oeste da escola.'),
-(8, 1, 'Corredor', 14, 15, 16, 5, 'Corredor conectando diferentes salas e áreas.'),
+(8, 1, 'Corredor', NULL, NULL, 21,23 , 'Corredor conectando diferentes salas e áreas.'),
 (9, 1, 'Refeitorio', 1, 13, 1, 1, 'Área para refeições e lanches dos alunos.'),
-(10, 1, 'Biblioteca', 13, 1, 1, 1, 'Biblioteca com várias seções de livros mágicos.'),
-(11, 1, 'Primeiro andar', 17, 18, 19, 13, 'Primeiro andar da escola, contendo várias salas.'),
-(12, 1, 'Sala de aula 1', 1, 16, 1, 1, 'Sala de aula utilizada para disciplinas iniciais.'),
-(13, 1, 'Sala de aula 2', 16, 1, 1, 1, 'Segunda sala de aula, usada para aulas avançadas.'),
-(14, 1, 'Sala de aula 3', 1, 1, 1, 16, 'Terceira sala de aula, especializada em feitiços.'),
+(10, 1, 'Biblioteca',30, NULL, NULL, 24, 'Biblioteca com várias seções de livros mágicos.'),
+(11, 3, 'Corredor maior',33,NULL, 32, 29, 'Primeiro andar da escola, contendo várias salas.'),
+(12, 1, 'Sala de aula de Defesa Contra as Artes das Trevas:', NULL, NULL, NULL,33, 'Sala de aula utilizada para disciplinas iniciais.'),
+(13, 1, 'Sala de aula de Poções', NULL, NULL, 33, NULL, 'Segunda sala de aula, usada para aulas avançadas.'),
+(14, 1, 'Sala de aula de Herbologia', NULL, NULL, NULL, 34, 'Terceira sala de aula, especializada em feitiços.'),
 (15, 1, 'Dormitório: Grifinória', 1, 1, 1, 12, 'Alojamento dos alunos da casa Grifinória.'),
 (16, 1, 'Dormitório: Cornival', 1, 1, 12, 1, 'Alojamento dos alunos da casa Cornival.'),
 (17, 1, 'Dormitório: Sonserina', 1, 1, 1, 11, 'Alojamento dos alunos da casa Sonserina.'),
 (18, 1, 'Dormitório: Lufa-lufa', 1, 1, 11, 1, 'Alojamento dos alunos da casa Lufa-lufa.'),
 (19, 2, 'Floresta Negra', 2, 7, 14, 2, 'Floresta densa com criaturas e segredos perigosos.'),
-(20, 2, 'Campo de Quadribol', 3, 6, 12, 3, 'Campo para o esporte popular dos bruxos, o Quadribol.');
+(20, 2, 'Campo de Quadribol', 3, 6, 12, 3, 'Campo para o esporte popular dos bruxos, o Quadribol.'),
+(21,1, 'Recepção',4,2,22,8,'REGERGER'),
+(22,1,'Escadas Moveis',32,NULL,NULL,21,'GERGE'),
+(23,1,'Sala de troféus',24,25,8,NULL,'ERGER'),
+(24,1,'Sala de estudos',26,23,10,NULL,'RGEGE'),
+(25,1,'Sala de Esgrima',23,NULL,NULL,NULL,'GREG'),
+(26,1,'Corredor',27,24,28,NULL,'GERG'),
+(27,1,'Hospital',NULL,26,NULL,NULL,'GREG'),
+(28,1, 'Banheiro',NULL,NULL,NULL,26,'GREG'),
+(29,3, 'Corredor',NULL,NULL,11,30,'GRE'),
+(30,1, 'Escadas',29,10,NULL,NULL,'ERG'),
+(31,3, 'Corredor Maior',NULL,34,36,37,'GRG'),
+(32,3, 'Corredor',NULL,22,NULL,11,'RGEG'),
+(33,3, 'Corredor Maior',34,11,12,13,'FWEFWEF'),
+(34,3, 'Corredor Maior',31,33,14,35,'ffjweifwe'),
+(35,3, 'Sala de Aula de História da Magia',NULL,NULL,34,NULL,'FJEUJFEW'),
+(36,3, 'Sala de Aula de Feitiços',NULL,NULL,NULL,31,'edfewdwe'),
+(37,3, 'Sala de Aula de Transfiguração',NULL,NULL,31,NULL,'edfwefef');
 
 
 -- Personagem 
