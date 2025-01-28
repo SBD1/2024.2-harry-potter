6d0db828a76df9e0ed9d3e36e7376ff026d146d5
pedro <pedrinhfm@gmail.com>
2025-01-28 16:49:06 -0300

    feat: adding functions and logical details in game.py to make the game works and create an exclusive file to store the texts

--- app/game.py
+++ app/game.py
@@ -1,6 +1,9 @@
 #inicializar o jogo no terminal para o usuário
+import random
+
 from database import Database
 from classes import *
+from texts import *
 import sys
 import os
 import platform
@@ -37,24 +40,23 @@ class Game:
             '|__| |__||__| |__||___|  |_||___|  |_|  |___|      |___|    |_______|  |___|    |___|  |_______||___|  |_|    |_|   |_||_______||______|\n\n')
 
         option = 0
-        while option !=3:
-            print("Bem-vindo(a) ao jogo!\n")
+        print("Bem-vindo(a) ao jogo!\n")
 
-            print('1 - Criar Novo Personagem\n' +
-                  '2 - Carregar Personagem\n' +
-                  '3 - Sair\n\n\n')
+        print('1 - Criar Novo Personagem\n' +
+              '2 - Carregar Personagem\n' +
+              '3 - Sair\n\n\n')
 
-            print('Digite a opção desejada: \n')
-            option = input()
-            if option == '1':
-                self.create_character()
-            elif option == '2':
-                self.load_character()
-            elif option == '3':
-                sys.exit()
-            else:
-                print('Opção inválida!\n')
-                self.start()
+        print('Digite a opção desejada: \n')
+        option = input()
+        if option == '1':
+            self.create_character()
+        elif option == '2':
+            self.load_character()
+        elif option == '3':
+            sys.exit()
+        else:
+            print('Opção inválida!\n')
+            self.start()
 
 
     def create_character(self):
@@ -74,21 +76,85 @@ class Game:
         print('Digite o nome do seu personagem: ')
         name = input()
         self.player = Database.load_character(self.connection, name)
-        if self.player.id_character == -1:
+        if not self.player:
             print('Personagem não encontrado!\n')
         else:
             self.new_game()
 
 
     def new_game(self):
-        while True:
-            Database.get_area_description(self.connection, self.player)
-            Database.get_areas(self.connection, self.player)
+        clear()
+        print(f' Bem-vindo(a) {self.player.name}!\n'
+              f'Você é o mais novo aluno da Escola de Magia e Bruxaria de Hogwarts.\n'
+              f'Está na hora de começar sua jornada!\n'
+              f'Boa sorte!\n\n\n')
 
+        while True:
+            self.get_current_area()
+            self.get_possibles_directions()
             direction = input()
-            #fazer ele andar
-            Database.change_area(self.connection, self.player, direction)
-            clear()
+            if direction == '2':
+                #fazer a dinâmica de escolher a casa
+                self.move_character(direction)
+                self.choice_house()
+                self.press_key_to_continue()
+                clear()
+                print(texto_pos_selecao)
+                self.press_key_to_continue()
+                print(texto_inicial_sobre_o_artefato)
+                print("Assim, curioso, você foi dormir, pronto para o primeiro dia de aula em Hogwarts.\n")
+                print("O dia amanheceu e o seu primeiro dia de aula em hogwarts começou!")
+                if self.player.idHouse == 1: #grifinória
+                    idarea = 15
+                elif self.player.idHouse == 3: #corvinal
+                    idarea = 16
+                elif self.player.idHouse == 2: #sonserina
+                    idarea = 17
+                elif self.player.idHouse == 4: #lufa-lufa
+                    idarea = 18
+                Database.set_area(self.connection, self.player, idarea)
+                break
+            else:
+                clear()
+                print('Você não pode ir para essa direção ainda!\n')
+
+        self.continue_game()
+
+    def choice_house(self):
+        self.get_current_area()
+        print(texto_de_entrada)
+        house = random.randint(1, 4)
+        self.player.idHouse = house
+        Database.set_house(self.connection, self.player)
+        print(f"E o chapéu seletor disse:\n 'Que seja: {Database.get_house(self.connection, self.player)}'\n"
+              f"E toda a mesa da casa entrou em euforia com a sua seleção!\n")
+
+    def continue_game(self):
+        clear()
+        self.get_current_area()
+        self.get_possibles_directions()
+
+
+
+
+
+
+    def press_key_to_continue(self):
+        print("Pressione qualquer tecla para continuar...\n")
+        input()
+
+    def get_current_area(self):
+        print(f'Você está na área: {Database.get_area_name(self.connection, self.player)}')
+
+        Database.get_area_description(self.connection, self.player)
+        print('\n\n')
+
+    def get_possibles_directions(self):
+        Database.get_areas(self.connection, self.player)
+
+    def move_character(self, direction):
+        Database.move(self.connection, self.player, direction)
+        clear()
 
 
 
---
+++ app/texts.py
@@ -0,0 +1,39 @@
+texto_de_entrada = """
+Você atravessa as portas gigantescas do Salão Principal e é imediatamente tomado por uma sensação de encanto
+O teto, enfeitiçado para refletir o céu noturno, brilha com estrelas que parecem tão próximas que você quase pode tocá-las.
+Velas flutuam suavemente pelo ar, iluminando o salão com uma luz quente e mágica.
+À sua frente, centenas de estudantes espalhados pelas mesas das Casas olham curiosos enquanto você caminha hesitante até o centro. 
+O Chapéu Seletor, velho e surrado, repousa sobre um banquinho solitário, esperando por você. O silêncio cresce. 
+Sua aventura em Hogwarts está prestes a começar.
+"""
+
+texto_pos_selecao = """
+A cerimônia de seleção terminou, e agora você se encontra no Grande Salão, saboreando sua primeira refeição como um estudante de Hogwarts.
+O som das conversas e risadas dos outros alunos preenche o ambiente, e você sente a excitação do início dessa nova jornada. 
+À medida que se acomoda à sua nova casa, começa a fazer amizades com seus colegas.
+De repente, uma conversa ao seu lado chama sua atenção. 
+Um estudante de Corvinal, com um olhar intrigado, sussurra para um amigo: 
+"Você ouviu falar sobre o artefato? Aquele que os fundadores criaram para proteger Hogwarts... dizem que foi perdido há séculos, mas que ainda há quem o procure." 
+O murmúrio é rápido, mas a ideia de algo tão poderoso, tão antigo, o deixa com uma sensação estranha. 
+Algo te diz que esse artefato, perdido no tempo, tem mais a ver com você do que imagina.
+"""
+
+texto_inicial_sobre_o_artefato = """
+Enquanto você digere a informação, a conversa ao seu redor começa a ficar mais abafada, até que o som do diretor se levantando preenche o salão. 
+Todos os olhares se voltam para ele. "A noite está avançando, meus jovens bruxos", ele diz com um sorriso acolhedor, mas com um tom de autoridade. 
+"É hora de se retirarem para seus dormitórios. Amanhã será um novo dia cheio de descobertas.\n"
+
+O murmúrio entre os alunos cresce, mas logo o Grande Salão começa a se esvaziar, com cada casa se dirigindo para suas respectivas torres. 
+No caminho para o dormitório, você percebe mais sussurros sobre o artefato — como se a menção dele tivesse despertado uma curiosidade em todos. 
+Algo no ar, uma energia estranha, faz você se perguntar: será que há algo mais por trás dessas histórias?
+Enquanto caminha pelos corredores, você cruza com um estudante mais velho, Alistair Blackwood, que parece notar seu olhar pensativo. 
+"Ouvi sua conversa mais cedo... Sobre o artefato?" Ele faz uma pausa, olhando ao redor antes de se aproximar com um sorriso misterioso. 
+"Não é algo fácil de se ignorar, eu sei. Mas cuidado com o que pergunta. A busca por ele não é um caminho simples.\n"
+
+Intrigado, você decide procurar Alistair novamente no dormitório. Lá, com o ambiente mais calmo, você o encontra e pergunta diretamente sobre o que sabe. 
+Ele hesita por um momento, como se ponderando suas palavras, antes de começar a explicar. 
+"O artefato não é apenas uma lenda. Criado pelos fundadores de Hogwarts, ele tem o poder de proteger a escola... ou destruir tudo o que conhecemos. 
+Há muitos que buscam seu poder, mas nem todos são dignos de controlá-lo. E o mais perigoso, é que ele pode escolher quem o controla." 
+A conversa deixa você com a sensação de que a busca por esse artefato vai muito além do que qualquer um imagina. 
+E, de alguma forma, você sente que está prestes a se envolver em algo muito maior do que imaginava.\n
+"""
