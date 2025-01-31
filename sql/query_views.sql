9be89f1b43bbb6d344a2d1ba838fbb98e32443bc
andre-maia51 <acmc.0410@gmail.com>
2025-01-31 18:18:57 -0300

    feat: view para relacionar feitiços com habilidade e professor

--- sql/query_views.sql
+++ sql/query_views.sql
@@ -31,3 +31,13 @@ FROM Inimigo i
 JOIN Area a ON i.idArea = a.idArea
 JOIN Regiao r ON a.idRegiao = r.idRegiao
 JOIN Mapa m ON r.idMapa = m.idMapa;
+
+-- View para relacionar feitiços as habilidades necessárias e aos professores que os ensinam
+CREATE VIEW vFeiticosHabilidade AS
+SELECT
+	f.nomeFeitico AS feitico,
+	h.nome AS habilidadeRequerida,
+	p.nome AS professorResp
+FROM Feitico f
+JOIN Habilidade h ON f.habilidadeRequerida = h.idHabilidade
+JOIN Professor p ON f.idProfessor = p.idProfessor;
